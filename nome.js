5450af9c46db808fd03b59dd40387eff5ce7977b
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-23 11:50:17 -0300

    Add files via upload

--- nome.js
+++ nome.js
@@ -9,6 +9,15 @@ let nome3 = "Julia Naiara"
 
 let inteiro = 17;
 let flutuante = 1.69;
+let inteiro2 = 61;
+let inteira3 = 6;
 
-console.log(`O nome dela é ${nome},o segundo nome da ${nome} é ${nome2}, o sobrenome da ${nome} é ${nomes}. Ela tem ${inteiro} anos, e tem ${flutuante} de altura.
+console.log(`O nome dela é ${nome},o segundo nome da ${nome} é ${nome2}, o sobrenome da ${nome} é ${nomes}. Ela tem ${inteiro} anos, e tem ${flutuante} de altura, e pesa ${inteiro2} kg 
 Ela é mais velha que a ${nome3}.`)
+
+//Adicione uma variável com um numero de 1 a 10.
+//E somem esse número na idade da pessoa.
+//Descrevam o peso e altura.
+
+console.log(`A idade é ${inteiro + inteira3} `)
+
