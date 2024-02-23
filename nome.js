24ec41ef74e5fa5bd74bce77a78c7a706194c4e5
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-23 11:38:19 -0300

    Add files via upload

--- nome.js
+++ nome.js
@@ -1,3 +1,7 @@
+//Um código que contenha as variáveis de nome e idade
+//Imprima-os no console, em uma única linha 
+//Usando os template litteras
+
 let nome = "Jélia"
 let nome2 = "Nichole"
 let nomes = "Roese"
@@ -8,5 +12,3 @@ let flutuante = 1.69;
 
 console.log(`O nome dela é ${nome},o segundo nome da ${nome} é ${nome2}, o sobrenome da ${nome} é ${nomes}. Ela tem ${inteiro} anos, e tem ${flutuante} de altura.
 Ela é mais velha que a ${nome3}.`)
-
-
---
+++ number.js
@@ -0,0 +1,12 @@
+//number é um tipo de dados numérico
+//sendo tanto inteiros (int) e ponto flutuante (float)
+
+let inteiro = 17;
+let flutuante = 1.69;
+let inteiro2 = 18
+let flutuante2 = 1.70;
+console.log(inteiro);
+console.log(flutuante);
+console.log(inteiro2);
+console.log(flutuante2);
+
