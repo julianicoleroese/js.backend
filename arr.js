76889db3c674d0d133857c642a2bf627a611ed32
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-26 11:35:51 -0300

    Add files via upload

--- arr.js
+++ arr.js
@@ -6,3 +6,5 @@ let nomes = ["marcio", "mercio", "mircio", "morcio", "murcio"]
 let idade = [11, 23, 32, 45, 67]
 
 console.log(`O ${nomes[0]} possui ${idade[0]}`)
+
+
---
+++ soma.js
@@ -0,0 +1,6 @@
+let soma = 5 + 3;
+let diferenca = 5 - 3;
+let multiplicacao = 5 * 3;
+let divisao = 10 / 2;
+let resto = 10 % 3;
+console.log(`A soma de 5+3 é ${soma}`)
