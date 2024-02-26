159256b9341601230f1a7c706e2f52c9d6102be8
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-26 11:19:37 -0300

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,8 @@
+//fazer duas listas, com 5 valores dentro de cada
+//uma com os nomes, e outro com idade
+//exibir na tela o nome junto da idade
+
+let nomes = ["marcio", "mercio", "mircio", "morcio", "murcio"]
+let idade = [11, 23, 32, 45, 67]
+
+console.log(`O ${nomes[0]} possui ${idade[0]}`)
