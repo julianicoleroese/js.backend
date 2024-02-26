ec8604ea6aed472a0ca145b4a1343d994333ecf7
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-26 10:47:20 -0300

    Add files via upload

---
+++ bool.js
@@ -0,0 +1,11 @@
+//let num1 = 26
+//let num2 = 26
+//console.log (`seu resultao foi de ${num1 == num2}`)
+
+let lobo  = {
+    nome: "el lobo",
+    raca: "apaixonado",
+    porte: "tem",
+    bonito: "claro",
+}
+console.log(lobo)
---
+++ obj.js
@@ -0,0 +1,12 @@
+//printe na tela todo um cadastro de uma pessoa
+//usando o tipo de dado object, exibindo os atributos:
+//nome, documento, estado civil, idade, endereço
+
+let cadastro  = {
+    nome: "Julia",
+    idade: "17",
+    documento: "090.454.269-60",
+    estadocivil: "Solteira",
+    endereço: "Porto Mendes",
+}
+console.log(`O nome do(a) cadastrante é ${nome} e possui ${idade}, o CPF informado é ${documento}, seu estado civil é ${estadocivil} e reside em ${endereço}`)
