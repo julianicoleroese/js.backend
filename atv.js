480b8d74dbf322b1a9810d2c287c7d597b0119e7
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-03-01 11:44:28 -0300

    Add files via upload

---
+++ atv.js
@@ -0,0 +1,48 @@
+//string
+let nome = 'Julia';
+//number 
+idade = 17;
+altura = 1.69;
+//boolean
+casada = false;
+console.log(nome,idade,altura,casada)
+
+
+
+
+//object
+pessoa = {
+    nome: 'Julia',
+    idade: 17,
+    casada: true,
+    altura: 1.69,
+}
+console.log(pessoa.nome, pessoa.idade ,pessoa.altura , pessoa.casada)
+
+
+
+
+//array
+let array = [15, 28, 6, 54, 27, 47]
+console.log(array[0]);
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+console.log(array[5])
+
+
+
+
+//operadores aritméticos
+// + ex:
+let soma = 2 + 4;
+// - ex:
+let subtracao = 56 - 87;
+// * ex:
+let multiplicacao = 4 * 9;
+// % ex:
+let restoDivisao = 10 % 3;
+console.log(soma, subtracao, multiplicacao, restoDivisao)
+
+
