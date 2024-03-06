ad2cde1c7f975de0b37989e74b12481a1da3f4ee
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-03-06 14:26:37 -0300

    Add files via upload

--- atv2.js
+++ atv2.js
@@ -1,31 +1,31 @@
 //faça uma sequência de 3 somas, onde o resultado séra 15.350
 let soma1= 5.000 + 5.000;
-let soma2= 10.000 + 5.000;
-let soma3= 15.000 + 0.350;
+let soma2= soma1 + 5.000;
+let soma3= soma2 + 0.350;
 console.log(soma3.toFixed(3));
 
 //faça uma sequência de 5 subtrações, onde o resultado séra -567
 let subtracao1 = 0 - 100;
-let subtracao2 = -100 - 200;
-let subtracao3 = -300 - 100;
-let subtracao4 = -400 - 100; 
-let subtracao5 = -500 - 67;
+let subtracao2 = subtracao1 - 200;
+let subtracao3 = subtracao2 - 100;
+let subtracao4 = subtracao3 - 100; 
+let subtracao5 = subtracao4 - 67;
 console.log(subtracao5);
 
 //faça uma sequência de 2 multiplicações, onde o resultado séra 980
 let multiplicacao1 = 245 * 2;
-let multiplicacao2 = 490 * 2;
+let multiplicacao2 = multiplicacao1 * 2;
 console.log(multiplicacao2);
 
 //faça uma sequência de 3 divisões, onde o resultado será 21
 let divisao1 = 504 / 2;
-let divisao2 = 252 / 4;
-let divisao3 = 63 / 3;
+let divisao2 = divisao1 / 4;
+let divisao3 = divisao2 / 3;
 console.log(divisao3);
 
 //faça uma sequência de resto de 4 divisões, onde resultado será 56
 let resto1 = 911 % 456;
-let resto2 = 455 % 228;
-let resto3 = 227 % 114;
-let resto4 = 113 % 57;
+let resto2 = resto1 % 228;
+let resto3 = resto2 % 114;
+let resto4 = resto3 % 57;
 console.log(resto4);
