47f703045a68e4d1e51eb919ed7fc2ad1f854465
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-03-07 09:12:45 -0300

    atv3.js

---
+++ atv3.js
@@ -0,0 +1,18 @@
+idade = 18
+cnh = true
+autorizaçao = true
+
+if (idade >= 18 && cnh == true && autorizaçao == true){
+    console.log('pode dirigir e entrar em propriedade privada')
+}
+else if (idade < 18 && cnh == false && autorizaçao == true){
+    console.log('nao pode dirigir e pode entrar em propriedade privada')
+}
+else if (idade >= 18 && cnh == true && autorizaçao == false){
+    console.log('pode dirigir e nao pode entrar em propriedade privada')
+}
+
+else{
+    console.log('nao pode dirigir e nem entrar em propriedade privada')
+}
+
